fad3bc49ed41f087d07a8b13b1aac4a074b208f8
Pedro Lucas <pedro.lucasrv@outlook.com>
2022-07-04 20:29:40 -0300

    testes com cpf

--- js/main.js
+++ js/main.js
@@ -34,29 +34,6 @@ const resetForm = (e) => {
     document.querySelector("form").reset(); 
 }
 
-const submitForm = (e) => {
-    e.preventDefault();
-
-
-
-    
-
-    let data = {
-        name: validate("#name"),
-        cpf: validate("#cpf"),
-        nascimento: validate( "#born-date" ),
-        genero:  validate("input[name='gender']:checked"),
-        email: validate( "#email" ),
-        senha: validate( "#password" ),
-        senhaConfirm: validatePassword( "#password-confirm" )
-    }
-
-    
-    
-    var $nome = $( "#name" ).val();
-
-    console.log(data)
-};
 
 const validate = (val) => {
 
@@ -71,10 +48,10 @@ const validate = (val) => {
 }
 
 const validatePassword = (val) => {
-
+    
     password = $('#password').val();
     input = $(`${val}`).val();
-
+    
     if (input){
         if(input === password){
             return true;
@@ -86,5 +63,65 @@ const validatePassword = (val) => {
         $(`${val}`).next().addClass('d-block');
         return false;
     }
+    
+}
 
+const validateCpf = (val) => {
+
+    var lenght = document.getElementById(`${val}`).innerHTML.length;
+    
+    console.log(lenght)
+    if (lenght < 15 && lenght > 13){
+        return true;
+    }else{
+        return false;
+        }
+    }
+
+const submitForm = (e) => {
+    e.preventDefault();
+
+    let data = {
+        name: validate("#name"),
+        cpf: validateCpf("cpf"),
+        nascimento: validate( "#born-date" ),
+        genero:  validate("input[name='gender']:checked"),
+        email: validate( "#email" ),
+        senha: validate( "#password" ),
+        senhaConfirm: validatePassword( "#password-confirm" )
+    }
+    var tl = gsap.timeline;
+    
+    console.log(data.cpf)
+    
+    gsap.to('form', {y: 50, opacity: 0, duration: 1})
+    gsap.to('.heading-custom', {y: 50, opacity: 0, duration: 1})
+    gsap.to('form', {display: "none"})
+    gsap.to('.heading-custom', {display: "none"})
+    gsap.to('.congratulations', {display: 'block', opacity: 100, delay: 1},  )
+    if( data.name && data.cpf.lenght < 19  ){
+    }
+
+    console.log(data)
+};
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
 }
